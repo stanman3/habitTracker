42d9d5caa4fd1c9f989b9c9f019050666e1ef5fb
stanman3 <stanislav.manolov.1@gmail.com>
2026-08-22 17:42:25 +0300

    origins

--- backend/main.py
+++ backend/main.py
@@ -7,7 +7,11 @@ from database import engine
 app = FastAPI()
 models.Base.metadata.create_all(bind=engine)
 
-app.add_middleware(CORSMiddleware, allow_origins=["http://localhost:3000"], allow_credentials=True, allow_methods=["*"], allow_headers=["*"])
+app.add_middleware(CORSMiddleware, allow_origins=[
+        "http://localhost:3000",
+        "https://frontend-production-453a.up.railway.app/",
+        "https://habitlog.stanislavmanolov.com"
+    ], allow_credentials=True, allow_methods=["*"], allow_headers=["*"])
 
 app.include_router(auth.router)
 app.include_router(habits.router)
